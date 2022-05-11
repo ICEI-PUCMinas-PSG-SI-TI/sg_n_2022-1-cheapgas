de077d9356b7bdbc68e5f329866289b47fb3de65
Pedro-ppassos <81398209+Pedro-ppassos@users.noreply.github.com>
2022-05-11 19:41:58 -0300

    Delete codigo/JS directory

--- codigo/JS/js.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("nome").placeholder = " Digite seu nome";
-document.getElementById("telefone").placeholder = " Digite seu telefone";
--- codigo/JS/teste.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo'deu certo';
-
-?>
